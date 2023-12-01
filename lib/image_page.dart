bf374425d23fb4f50ce401547b2dbf514a3fd7f8
MinMint01 <mina.kizhakkemadom@gmail.com>
2023-12-01 16:49:12 +0530

    add message_page

--- lib/image_page.dart
+++ lib/image_page.dart
@@ -1,22 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:image_picker/image_picker.dart';
+import 'message_page.dart';
 
 class ImagePage extends StatelessWidget {
-  const ImagePage({super.key});
+  const ImagePage({super.key, Key? kkey});
 
-  Future<void> _captureImage() async {
+  Future<void> _captureImage(BuildContext context) async {
     try {
       final XFile? image =
           await ImagePicker().pickImage(source: ImageSource.camera);
 
       if (image == null) return; // User canceled the image capture
 
-      // Handle the captured image, for example, you can display it or upload it.
-      if (kDebugMode) {
-        print('Image captured: ${image.path}');
-      }
+      // Navigate to the MessagePage with the captured image
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MessagePage(
+            image: File(image.path),
+            message: '',
+          ),
+        ),
+      );
     } catch (e) {
       if (kDebugMode) {
         print('Error capturing image: $e');
@@ -47,7 +57,8 @@ class ImagePage extends StatelessWidget {
               icon: const Icon(Icons.camera),
               iconSize: 72.0,
               color: Colors.white,
-              onPressed: _captureImage,
+              onPressed: () =>
+                  _captureImage(context), // Pass the context to _captureImage
             ),
             const SizedBox(height: 20),
             // Text "TAP TO CAPTURE"
---
+++ lib/message_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MessagePage(
+        message: '',
+        image: null,
+      ),
+    );
+  }
+}
+
+class MessagePage extends StatelessWidget {
+  final String message;
+
+  const MessagePage({super.key, required this.message, required image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Message Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Transmitted Message: $message',
+              style: const TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Show a snackbar with the message
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Message: $message'),
+                  ),
+                );
+              },
+              child: const Text('Show Message'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
